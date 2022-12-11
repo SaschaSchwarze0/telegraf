eb2bd3e4f3a0ae5ca672609b2d7916d571437d44
Sascha Schwarze <schwarzs@de.ibm.com>
2022-12-11 14:17:47 +0100

    Enhance Kubernetes plugin - Enhance Node metrics by adding ephemeral storage capacity and conditions - Enhance Container metrics by adding resource usage

--- plugins/inputs/kube_inventory/pod_test.go
+++ plugins/inputs/kube_inventory/pod_test.go
@@ -9,6 +9,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metricsv1beta1 "k8s.io/metrics/pkg/apis/metrics/v1beta1"
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/metric"
@@ -446,7 +447,7 @@ func TestPod(t *testing.T) {
 		acc := new(testutil.Accumulator)
 		items := ((v.handler.responseMap["/pods/"]).(*corev1.PodList)).Items
 		for i := range items {
-			ks.gatherPod(&items[i], acc)
+			ks.gatherPod(&items[i], []metricsv1beta1.ContainerMetrics{}, acc)
 		}
 
 		err := acc.FirstError()
@@ -675,7 +676,7 @@ func TestPodSelectorFilter(t *testing.T) {
 		acc := new(testutil.Accumulator)
 		items := ((v.handler.responseMap["/pods/"]).(*corev1.PodList)).Items
 		for i := range items {
-			ks.gatherPod(&items[i], acc)
+			ks.gatherPod(&items[i], []metricsv1beta1.ContainerMetrics{}, acc)
 		}
 
 		// Grab selector tags
@@ -981,7 +982,7 @@ func TestPodPendingContainers(t *testing.T) {
 		acc := new(testutil.Accumulator)
 		items := ((v.handler.responseMap["/pods/"]).(*corev1.PodList)).Items
 		for i := range items {
-			ks.gatherPod(&items[i], acc)
+			ks.gatherPod(&items[i], []metricsv1beta1.ContainerMetrics{}, acc)
 		}
 
 		err := acc.FirstError()
